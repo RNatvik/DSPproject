862405ce6d185670be3521f0740c94c438d2de47
Ruben <r_bn-95@hotmail.com>
2019-11-24 22:45:31 +0100

    Added method for generating signals. Added method for adding signals together

--- filter/utilities.py
+++ filter/utilities.py
@@ -16,16 +16,42 @@ def get_frequency_response(b, a: list = None, k: float = 1, steps: int = 1001):
         aj = sum([ha * sin((1 - A + na) * w) for ha, na in zip(a, range(A))])
         bj = sum([hb * sin((1 - B + nb) * w) for hb, nb in zip(b, range(B))])
         H = complex(br, bj) / complex(ar, aj)
-        magnitude.append(k*abs(H))
+        magnitude.append(k * abs(H))
         phase.append(-atan2(H.imag, H.real))
     return magnitude, phase, W
 
 
+def generate_signal(f, fs, A, DC, t_final, t_steps=1000):
+    """
+    Generate a signal with specified specifications
+    :param f: the signal's frequency
+    :param fs: the sampling frequency
+    :param A: the sine wave amplitude
+    :param DC: an added dc component of the signal
+    :param t_final: final time for signal.
+    :param t_steps: number of steps for time vector
+    :return: x(n), t(n), x(t), t
+    """
+    w = 2 * pi * f / fs
+    T = [t_final * i / t_steps for i in range(t_steps)]
+    N = int(t_final * fs)
+    tn = [i / fs for i in range(N)]
+    xn = [A * sin(w * n) + DC for n in range(N)]
+    xt = [A * sin(2 * pi * f * t) + DC for t in T]
+    return xn, tn, xt, T
+
+
+def add_signals(sig1: list, sig2: list):
+    if len(sig1) != len(sig2):
+        return None
+    return [s1 + s2 for s1, s2 in zip(sig1, sig2)]
+
+
 def amplitude2db(amp: list):
-    db = [20*log(a, 10) for a in amp]
+    db = [20 * log(a, 10) for a in amp]
     return db
 
 
 def db2amplitude(db: list):
-    amp = [pow(10, (d/20)) for d in db]
+    amp = [pow(10, (d / 20)) for d in db]
     return amp
--- main.py
+++ main.py
@@ -1,47 +1,70 @@
-import filter as f
+import filter as fi
 from matplotlib import pyplot as plt
 
-num = [0.00025421634845499124,
-       -0.00079187872147285572,
-       0.00141377112114653320,
-       -0.00167945032088055311,
-       0.00176640706253605641,
-       -0.00167945032088055311,
-       0.00141377112114653320,
-       -0.00079187872147285572,
-       0.00025421634845499124]
 
-den = [1,
-       -6.142345166576267168068,
-       16.682363664620424970053,
-       -26.134169993147157384782,
-       25.804074652366246311885,
-       -16.430866660921950028750,
-       6.584876413720856191957,
-       -1.517738749625560545908,
-       0.153965563480438771826]
+def main():
+    num = [0.00025421634845499124,
+           -0.00079187872147285572,
+           0.00141377112114653320,
+           -0.00167945032088055311,
+           0.00176640706253605641,
+           -0.00167945032088055311,
+           0.00141377112114653320,
+           -0.00079187872147285572,
+           0.00025421634845499124]
 
-b = num
-a = den
+    den = [1,
+           -6.142345166576267168068,
+           16.682363664620424970053,
+           -26.134169993147157384782,
+           25.804074652366246311885,
+           -16.430866660921950028750,
+           6.584876413720856191957,
+           -1.517738749625560545908,
+           0.153965563480438771826]
 
-filt = f.Filter(b, a=a)
-step = [1] * 160
-impulse = [1] + [0] * 159
-step_response = filt.filter_list(step)
-filt.clear()
-impulse_response = filt.filter_list(impulse)
+    b = num
+    a = den
+    filt = fi.Filter(b, a=a)
 
-mag, phase, W = f.get_frequency_response(b, a=a)
-db = f.amplitude2db(mag)
+    step = [1] * 160
+    impulse = [1] + [0] * 159
+    Om = 0.3  # * pi (rad / sample)
+    fs = 100
+    f = Om / 2 * fs
+    print(f, f/10)
+    xn1, tn, xt1, t = fi.generate_signal(f, fs, 0.2, 0, 5)
+    xn2, tn2, xt2, t2 = fi.generate_signal(f / 10, fs, 1, 0, 5)
+    xn = fi.add_signals(xn1, xn2)
+    xt = fi.add_signals(xt1, xt2)
 
-plt.figure()
-plt.plot(W, db)
-plt.show()
+    step_response = filt.filter_list(step)
+    filt.clear()
+    impulse_response = filt.filter_list(impulse)
+    filt.clear()
+    yn = filt.filter_list(xn)
 
-plt.figure()
-plt.plot(step_response)
-plt.show()
+    mag, phase, W = fi.get_frequency_response(b, a=a)
+    db = fi.amplitude2db(mag)
 
-plt.figure()
-plt.plot(impulse_response)
-plt.show()
+    plt.figure()
+    plt.plot(W, db)
+
+    plt.figure()
+    plt.plot(W, phase)
+
+    plt.figure()
+    plt.plot(step_response)
+
+    plt.figure()
+    plt.plot(impulse_response)
+
+    plt.figure()
+    plt.plot(t, xt)
+    plt.plot(tn, yn)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
