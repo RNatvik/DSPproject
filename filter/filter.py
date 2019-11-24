00256cfa477c19358438993c9b3143777fa3bb47
Ruben <r_bn-95@hotmail.com>
2019-11-24 19:02:07 +0100

    Implemented basic functionality for a filter

--- filter/filter.py
+++ filter/filter.py
@@ -2,19 +2,29 @@
 
 class Filter:
 
-    def __init__(self, a: list, b: list, k: float = 1):
+    def __init__(self, a: list, b: list = None, k: float = 1):
         self.a = a
         self.b = b
         self.k = k
+        self.input = [0] * len(self.a)
+        if self.b:
+            self.output = [0] * len(self.b)
+        else:
+            self.output = [0]
 
-        self.prev_in = [0]*len(a)
-        self.prev_out = [0]*len(b)
+    def filter_value(self, x_new: float):
+        self.input = self._shift_list(self.input, x_new)
+        y_new = sum([a*x for a, x in zip(self.a, self.input)]) + sum([b*y for b, y in zip(self.b, self.output)])
+        self.output = self._shift_list(self.output, y_new)
+        return y_new
 
-    def filter_value(self, x):
-        pass
-
-    def filter_list(self, xn):
-        pass
+    def filter_list(self, xn: list):
+        y = []
+        for x in xn:
+            y.append(self.filter_value(x))
+        return y
 
     def _shift_list(self, lst: list, val: float):
-        pass
+        lst.pop()
+        lst.insert(0, val)
+        return lst
